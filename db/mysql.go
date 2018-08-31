49c58f82eeb1730e2c9068c92007999496fc1596
Benjamin Chris <loli@lurenjia.in>
2018-08-31 20:27:56 +0800

    【新增】数据库兼容层

---
+++ db/mysql.go
@@ -0,0 +1,6 @@
+// Token-Static-Center
+// 数据库模块-MySQL兼容层
+// 针对MySQL进行兼容
+// LiuFuXin @ Token Team 2018 <loli@lurenjia.in>
+
+package db
---
+++ db/sqlite.go
@@ -0,0 +1,6 @@
+// Token-Static-Center
+// 数据库模块-SQLite兼容层
+// 针对SQLite进行兼容
+// LiuFuXin @ Token Team 2018 <loli@lurenjia.in>
+
+package db
