ace5ef4559a5aa113e7e6aa7963b4a2c86a4e01a
Benjamin Chris <loli@lurenjia.in>
2018-08-31 20:29:29 +0800

    【新增】防盗链相关配置

--- security/security.go
+++ security/security.go
@@ -7,15 +7,82 @@ package security
 
 import (
 	"net/http"
-	"fmt"
+	"github.com/LuRenJiasWorld/Token-Static-Center/util"
+	"github.com/LuRenJiasWorld/Token-Static-Center/app"
 )
 
-// 过滤白名单以外的IP访问
+// 过滤白名单以外的域名访问图片资源
 func WhiteListFilter(next http.Handler) (http.Handler) {
 	handlerFunction := func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println(r.Referer())
+		// 获取防盗链配置
+		antiLeechStatus, err := util.GetConfig("Security", "AntiLeech", "Status")
+
+		if err != nil {
+			util.ErrorLog("security", "读取防盗链配置失败，错误信息为：" + err.Error(), "security->WhiteListFilter")
+			return
+		}
+
+		// 转换Interface到String
+		antiLeechStatus = antiLeechStatus.(string)
+
+		// 如果防盗链开启
+		if antiLeechStatus == "on" {
+			// 获取防盗链白名单
+			whiteListInterface, err := util.GetConfig("Security", "WhiteList")
+			if err != nil {
+				util.ErrorLog("security", "无法获取防盗链白名单，请检查防盗链配置！，错误信息：" + err.Error(), "security->WhiteListFilter")
+				return
+			}
+
+			// 转换Interface到StringSlice
+			whiteListArray := whiteListInterface.([]string)
+
+			// 获取当前的HTTP referrer
+			requestReferrer := r.Referer()
+
+			// httpReferrer是否命中白名单
+			hitFlag := false
+			for i := 0; i < len(whiteListArray); i++ {
+				if whiteListArray[i] == requestReferrer {
+					hitFlag = true
+					break
+				}
+			}
+
+			// 获取防盗链警告配置
+			antiLeechWarning, err := util.GetConfig("Security", "AntiLeech", "ShowWarning")
+
+			if err != nil {
+				util.ErrorLog("security", "读取防盗链警告配置失败，错误信息为：" + err.Error(), "security->WhiteListFilter")
+			}
+
+			// 转换Interface到String
+			antiLeechWarning = antiLeechWarning.(string)
+
+			// 如果未命中白名单，或者根本没有来源头，返回错误
+			if hitFlag == false || requestReferrer == "" {
+				switch antiLeechWarning {
+					case "on":
+						app.AntiLeechImage(w, r)
+						break
+					case "off":
+						app.ErrorPage(w, r, 403)
+						break
+				}
+				return
+			}
+
+		}
+
+		// 传递到下一个中间件
 		next.ServeHTTP(w, r)
 	}
 
 	return http.HandlerFunc(handlerFunction)
+}
+
+
+// 安全上传
+func SecureUploadFilter() {
+
 }
