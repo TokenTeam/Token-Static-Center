756f78f5d6c692e5fdea21182ce8015cd9c76db7
Benjamin Chris <loli@lurenjia.in>
2018-09-10 19:06:19 +0800

    【新增】缓存模块-缓存获取逻辑

--- app/cache.go
+++ app/cache.go
@@ -6,8 +6,26 @@
 package app
 
 // 缓存获取
-func CacheFetchHandler() () {
+func CacheFetchHandler(cacheFileName string) (cacheStatus bool, cacheFileStream []byte, fileSizeByte int, err error) {
+	// 获取缓存路径
+	cachePath, err := getCachePath(cacheFileName)
+	if err != nil {
+		return false, nil, -1, errors.New("尝试获取缓存时遭遇致命错误：" + err.Error())
 
+	}
+
+	// 尝试读取文件
+	cacheFileStream, err = readFile(cachePath)
+	// 处理错误
+	// 文件不存在，或者文件为文件夹（不可能为文件夹，此前已经过滤过了）
+	if err != nil {
+		return false, nil, -1, nil
+	}
+
+	// 获取文件体积
+	cacheFileSize := bytes.Count(cacheFileStream, nil)
+
+	return true, cacheFileStream, cacheFileSize, nil
 }
 
 // 缓存写入
