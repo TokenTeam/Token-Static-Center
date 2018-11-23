a1d5f7c7abd39df0a007cdd819be57fcf1fcaa32
Benjamin Chris <loli@lurenjia.in>
2018-11-23 22:56:00 +0800

    【fix】修复Docker无法正常加载可执行文件的BUG

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,7 +2,7 @@
 # Init Scripts
 rm -rf /etc/localtime && cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 mv -f /home/gopath/src/github.com/TokenTeam/Token-Static-Center/token-static-center /bin
-mv -f /home/gopath/src/github.com/TokenTeam/Token-Static-Center/template /home/htdocs
+mv -f /home/gopath/src/github.com/TokenTeam/Token-Static-Center/template /bin
 mv -n /home/gopath/src/github.com/TokenTeam/Token-Static-Center/static /home/htdocs
 chmod +x /bin/token-static-center
-cd /bin/token-static-center --config=/etc/token-static-center/config.yaml
+cd /bin && /bin/token-static-center --config=/etc/token-static-center/config.yaml
